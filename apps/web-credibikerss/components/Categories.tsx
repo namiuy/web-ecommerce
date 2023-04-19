ddb02da3922aa7a48ae795f116259f57184b777f
Ignacio Rodriguez <ignaciort91@gmail.com>
2023-04-18 22:26:23 -0300

    add carousel slidesPerView

--- apps/web-credibikerss/components/Categories.tsx
+++ apps/web-credibikerss/components/Categories.tsx
@@ -13,7 +13,7 @@ export const Categories = () => {
   if (isLoading) return <div>Loading...</div>; // TODO:!
 
   return (
-    <Carousel rows={2} slideWidth="10rem" slideHeight="10rem">
+    <Carousel rows={2} slideWidth="10rem" slideHeight="10rem" slidesPerView={1}>
       {data.map(({ name, path, image_url }, i) => (
         <Link key={i} href={path ? path : '/'}>
           <Image w="10rem" h="10rem" src={image_url} alt={name} />
