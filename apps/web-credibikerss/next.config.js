76ead99123d196b7110744bb637cc33704f7cc15
Ignacio Rodriguez <ignaciort91@gmail.com>
2023-06-02 20:27:13 -0300

    add bffUrl log

--- apps/web-credibikerss/next.config.js
+++ apps/web-credibikerss/next.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 
 const { APP_NAME, BFF_URL } = process.env;
-
+console.log('next.config', BFF_URL)
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['shared', 'ui'],
--- packages/shared/env.ts
+++ packages/shared/env.ts
@@ -3,6 +3,7 @@ import getConfig from 'next/config';
 const {
   publicRuntimeConfig: { bffUrl },
 } = getConfig();
+console.log('env', bffUrl);
 
 export const bff = {
   url: bffUrl,
