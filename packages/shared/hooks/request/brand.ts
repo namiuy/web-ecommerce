99f910f1526b9e69ec55abf4199836dffbcf86e1
Ignacio Rodriguez <ignaciort91@gmail.com>
2023-06-02 20:23:26 -0300

    add bffUrl log

--- packages/shared/hooks/request/brand.ts
+++ packages/shared/hooks/request/brand.ts
@@ -3,4 +3,7 @@ import { Brand } from '../../entities/brand';
 import { bff } from '../../env';
 import { Response } from './response';
 
-export const useBrandList = (): Response<Array<Brand>> => useRequestWithCache(`${bff.url}/brand`);
+export const useBrandList = (): Response<Array<Brand>> => {
+  console.log('bffUrl', bff, bff.url);
+  return useRequestWithCache<Array<Brand>>(`${bff.url}/brand`);
+};
