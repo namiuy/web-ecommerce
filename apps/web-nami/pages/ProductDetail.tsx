51139d0bfd450506542a1fe25abb9edbe1b3933c
lautaro-yaurreche <lautaroyaurreche@gmail.com>
2023-07-21 16:07:42 -0300

    Detalle terminado

--- apps/web-nami/pages/ProductDetail.tsx
+++ apps/web-nami/pages/ProductDetail.tsx
@@ -1,8 +1,13 @@
-import { Container } from '@chakra-ui/react';
-import { Image } from '@chakra-ui/react';
-import { Grid } from '@chakra-ui/react';
-import { GridItem } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
+import { Container, Image, Grid, GridItem, Text, Box, useDisclosure } from '@chakra-ui/react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
 import { Button } from 'ui';
 
 const jsonStr = {
@@ -26,6 +31,11 @@ const jsonStr = {
   ],
 };
 
+type RelatedLink = {
+  name: string;
+  url: string;
+};
+
 type Product = {
   code: string;
   name: string;
@@ -39,38 +49,80 @@ type Product = {
   category: {
     name: string;
   };
-  relatedLinks: [
-    {
-      name: string;
-      url: string;
-    },
-  ];
+  relatedLinks: RelatedLink[];
+};
+
+const ShowImage = (props: any) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Image onClick={onOpen} src={props.image} alt="Herramienta NAMI" cursor={'pointer'} />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent maxW={'70%'} maxH={'85%'}>
+          <ModalHeader>{props.brand}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={'auto'}>
+            <Image src={props.image} alt="Herramienta NAMI" />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={onClose}>
+              CERRAR
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 const ProductDetail = () => {
   const product: Product = jsonStr;
   return (
-    <Container maxW={'950px'} h={'550px'} border={'1px'} p={'5'} mt={'4'}>
+    <Container maxW={'950px'} border={'1px'} borderColor={'blackAlpha.200'} p={'2rem'} mt={'5rem'}>
       <Grid
-        templateAreas={`"image details" "image buy" "description description"`}
-        templateRows={'repeat(3, 1fr)'}
+        templateAreas={`"image details" "description description"`}
+        templateRows={'auto 1fr'}
         templateColumns={'repeat(2, 1fr)'}
         gap={4}
       >
-        <GridItem bg={'purple.100'} area={'image'} borderRight={'1px'} p={'1rem'}>
-          <Image src={product.image} alt="Herramienta NAMI" />
+        <GridItem area={'image'} borderRight={'1px'} borderColor={'blackAlpha.200'} pr={'3rem'}>
+          <ShowImage brand={product.brand.name} image={product.image} />
         </GridItem>
-        <GridItem bg="purple.100" area={'details'}>
-          <Text>{product.name}</Text>
-          <Text>{product.code}</Text>
-          <Text>{product.price} + IVA </Text>
+        <GridItem area={'details'} pl={'2rem'}>
+          <Box borderBottom={'1px'} borderColor={'blackAlpha.200'}>
+            <Text fontWeight={'extrabold'} fontSize={'1.3rem'}>
+              {product.name}
+            </Text>
+            <Text color={'blackAlpha.600'} fontSize={'0.8rem'}>
+              <Text as="span" fontSize={'0.6rem'}>
+                Cod{' '}
+              </Text>
+              {product.code}
+            </Text>
+            <Text pt={'1rem'} pb={'2.5rem'} fontSize={'2.2rem'}>
+              <Text as="span" fontSize={'1.7rem'}>
+                U$S{' '}
+              </Text>
+              {product.price}
+              <Text as="span" color={'blackAlpha.600'} fontSize={'0.6rem'}>
+                {' '}
+                + IVA
+              </Text>
+            </Text>
+          </Box>
+          <Box>
+            <Text color={'blackAlpha.600'} fontSize={'0.65rem'} pt={'2rem'} pb={'1rem'}>
+              Stock {product.stock === 'SI' ? 'Si' : 'No'}
+            </Text>
+            <Button width={'100%'} bg={'red.500'} color={'white'} _hover={{ bg: 'red.700' }}>
+              COMPRAR
+            </Button>
+          </Box>
         </GridItem>
-        <GridItem bg="purple.100" area={'buy'}>
-          <Text>{product.stock}</Text>
-          <Button>Comprar</Button>
-        </GridItem>
-        <GridItem bg="purple.100" borderTop={'1px'} area={'description'}>
-          <Text> {product.description} </Text>
+        <GridItem area={'description'} borderTop={'1px'} borderColor={'blackAlpha.200'}>
+          <Text pt={'1.5rem'}> {product.description} </Text>
         </GridItem>
       </Grid>
     </Container>
