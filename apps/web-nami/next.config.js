638eb4c65b972956d22614b158cabf330a97b35b
lautaro-yaurreche <lautaroyaurreche@gmail.com>
2023-09-04 15:34:21 -0300

    Issue #10 WhatsApp

--- apps/web-nami/next.config.js
+++ apps/web-nami/next.config.js
@@ -44,6 +44,27 @@ const socialNeworksItems = [
   },
 ];
 
+const branches = [
+  {
+    address: 'Bvr. Artigas 3397',
+    schedule: 'Lunes a Viernes: de 8:00 a 12:00 y de 13:30 a 18:30 hrs.',
+    whatsApp: {
+      number: 598098000600,
+      text: '098 000 600',
+    },
+    location: 'https://goo.gl/maps/8EZqjv8GDNFBBoAM9',
+  },
+  {
+    address: 'Cerro Largo 1518',
+    schedule: 'Lunes a Viernes: de 8:00 a 12:30 y de 13:30 a 18:00 hrs.',
+    whatsApp: {
+      number: 598091033282,
+      text: '091 033 282',
+    },
+    location: 'https://goo.gl/maps/9Uhq6dWkB9FrVQmp6',
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['shared', 'ui'],
@@ -52,6 +73,7 @@ module.exports = {
     bffUrl: BFF_URL,
     menuItems,
     socialNeworksItems,
+    branches,
     keys: {
       googleMapsApiKey: GOOGLE_MAP_API_KEY,
     },
