0f2e0276a70ebe3c8f44bfbaa6837a59eafa8b67
Lautaro Yaurreche <lautaroyaurreche@192.168.1.12>
2023-10-10 13:19:46 -0300

    Issue #19 Contact

--- packages/ui/templates/Contact.tsx
+++ packages/ui/templates/Contact.tsx
@@ -1,57 +1,122 @@
-import { Box, Container, Flex, Text } from '..';
+import { Link, Divider, Icon } from '@chakra-ui/react';
+import { Box, Container, Flex, Text, Map, Grid, GridItem } from 'ui';
+import { AiOutlineClockCircle, AiOutlineWhatsApp } from 'react-icons/ai';
+import { IoCallOutline, IoLocationOutline } from 'react-icons/io5';
 
 export const Contact = () => {
   return (
     <>
-      <Container mt="5rem" px="0" mb="3rem">
-        <Text fontSize="1.375rem" fontWeight="bold">
+      <Container mt="3.5rem" px="0" mb="1rem">
+        <Text fontSize="1.5rem" fontWeight="bold">
           CONTACTO
         </Text>
       </Container>
-      <Container py="1rem" mb="5rem" px="0">
-        <Flex justifyContent="space-between">
-          <Box
-            bg="blackAlpha.200"
-            h="15rem"
-            w="79%"
-            borderRadius="1rem"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
+      <Divider />
+      <Container my="2rem" px="0">
+        <Flex>
+          <Box w="50%">
+            <Map position={{ lat: -34.9001314, lng: -56.1847301 }} h="25rem" zoom={16} />
+          </Box>
+          <Grid
+            w="50%"
+            fontWeight="500"
+            fontSize="1.125rem"
+            bg="primary.main"
+            color="white"
+            p="1.5rem"
+            borderRadius="0 1rem 1rem 0"
+            gridTemplateAreas={`"a b" "c d"`}
+            gridTemplateColumns={`1fr 1fr`}
+            gridTemplateRows={`1fr 1fr`}
+            placeItems="center"
           >
-            <Text>Mapa</Text>
-          </Box>
-          <Box w="20%">
-            <Text pb="1rem">
-              Bvar. Artigas 3397 <br /> Casi Gral. Flores
-            </Text>
-            <Text pb="1rem">Lunes a Viernes: de 8:00 a 12:00 y de 13:30 a 18:30hrs.</Text>
-            <Text pb="1rem">2402 1350 - 22034381</Text>
-            <Text>098 000 600</Text>
-          </Box>
+            <GridItem gridArea="a" textAlign="center">
+              <Icon as={IoLocationOutline} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                DIRECCION
+              </Text>
+              <Text>
+                Bvar. Artigas 3397 <br /> Casi Gral. Flores
+              </Text>
+            </GridItem>
+            <GridItem gridArea="b" textAlign="center">
+              <Icon as={AiOutlineClockCircle} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                HORARIO
+              </Text>
+              <Text>Lunes a Viernes: de 8:00 a 12:00 y de 13:30 a 18:30hrs.</Text>
+            </GridItem>
+
+            <GridItem gridArea="c" textAlign="center">
+              <Icon as={IoCallOutline} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                TELEFONO
+              </Text>
+              <Text>2402 1350 - 2203 4381</Text>
+            </GridItem>
+
+            <GridItem gridArea="d" textAlign="center">
+              <Icon as={AiOutlineWhatsApp} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                WhatsApp
+              </Text>
+              <Text>098 000 600</Text>
+            </GridItem>
+          </Grid>
         </Flex>
       </Container>
-      <Container py="1rem" px="0">
-        <Flex justifyContent="space-between">
-          <Box w="20%" textAlign="right">
-            <Text pb="1rem">
-              Cerro Largo 1518 <br />
-              Esq. Piedra Alta
-            </Text>
-            <Text pb="1rem">Lunes a Viernes: de 8:00 a 12:30 y de 13:30 a 18:00hrs.</Text>
-            <Text pb="1rem">2402 0922 - 2402 0031</Text>
-            <Text>091 033 282</Text>
-          </Box>
-          <Box
-            bg="blackAlpha.200"
-            h="15rem"
-            w="79%"
-            borderRadius="1rem"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
+      <Divider />
+      <Container my="2rem" px="0">
+        <Flex>
+          <Grid
+            w="50%"
+            fontWeight="500"
+            fontSize="1.125rem"
+            bg="primary.main"
+            color="white"
+            p="1.5rem"
+            borderRadius="1rem 0 0 1rem"
+            gridTemplateAreas={`"a b" "c d"`}
+            gridTemplateColumns={`1fr 1fr`}
+            gridTemplateRows={`1fr 1fr`}
+            placeItems="center"
           >
-            <Text>Mapa</Text>
+            <GridItem gridArea="a" textAlign="center">
+              <Icon as={IoLocationOutline} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                DIRECCION
+              </Text>
+              <Text>
+                Cerro Largo 1518 <br />
+                Esq. Piedra Alta
+              </Text>
+            </GridItem>
+            <GridItem gridArea="b" textAlign="center">
+              <Icon as={AiOutlineClockCircle} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                HORARIO
+              </Text>
+              <Text>Lunes a Viernes: de 8:00 a 12:30 y de 13:30 a 18:00hrs.</Text>
+            </GridItem>
+
+            <GridItem gridArea="c" textAlign="center">
+              <Icon as={IoCallOutline} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                TELEFONO
+              </Text>
+              <Text>2402 0922 - 2402 0031</Text>
+            </GridItem>
+
+            <GridItem gridArea="d" textAlign="center">
+              <Icon as={AiOutlineWhatsApp} w={'2.5rem'} h={'2.5rem'} />
+              <Text fontSize="1.25rem" fontWeight="bold" mb="0.25rem">
+                WhatsApp
+              </Text>
+              <Text>091 033 282</Text>
+            </GridItem>
+          </Grid>
+          <Box w="50%">
+            <Map position={{ lat: -34.9001314, lng: -56.1847301 }} h="25rem" zoom={16} />
           </Box>
         </Flex>
       </Container>
