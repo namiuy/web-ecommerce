ecc41fa951650536ee5159923b32a902b0848828
Lautaro Yaurreche <lautaroyaurreche@hotmail.com>
2024-03-22 16:03:04 -0300

    added ga initializer

--- apps/web-nami/pages/_app.tsx
+++ apps/web-nami/pages/_app.tsx
@@ -5,6 +5,15 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AppContextProvider } from 'shared';
 import { ThemeProvider, WhatsApp } from 'ui';
 import { theme } from '../theme/index';
+import ReactGA from 'react-ga4';
+import { keys } from 'shared';
+
+const { googleGaMeasurementId } = keys;
+
+if (googleGaMeasurementId) {
+  console.log('ReactGA initialized');
+  ReactGA.initialize(googleGaMeasurementId);
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
