dc96b828098ae4da8e1e2e67a4beb7e8ebd79e4d
Lautaro Yaurreche <lautaroyaurreche@hotmail.com>
2024-06-27 15:45:21 -0300

    added policies page

--- packages/shared/hooks/analytics.ts
+++ packages/shared/hooks/analytics.ts
@@ -1,7 +1,16 @@
 import { useRouter } from 'next/router';
 import ReactGA from 'react-ga4';
 
-export type Page = 'SignIn' | 'Home' | 'Company' | 'Products' | 'ProductDetail' | 'Brand' | 'Contact' | 'Register';
+export type Page =
+  | 'SignIn'
+  | 'Home'
+  | 'Company'
+  | 'Products'
+  | 'ProductDetail'
+  | 'Brand'
+  | 'Contact'
+  | 'Register'
+  | 'Policies';
 
 type Category = 'Cart' | 'Quote';
 
