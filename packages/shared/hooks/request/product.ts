615e57ab640a6d43ba3c5ee0f4715a6bd42928fb
Lautaro Yaurreche <lautaroyaurreche@gmail.com>
2025-05-16 13:41:01 -0300

    feature/images

--- packages/shared/hooks/request/product.ts
+++ packages/shared/hooks/request/product.ts
@@ -21,8 +21,9 @@ export const productAdd = (data: Product): Promise<Product> => {
   return post<Product>(`${bff.url}/products`, { body: JSON.stringify(data) }, true);
 };
 
-export const productUpdate = (id: string, data: any): Promise<Product> =>
-  put<Product>(`${bff.url}/products/${id}`, { body: JSON.stringify(data) }, true);
+export const productUpdate = (id: string, data: any): Promise<Product> => {
+  return put<Product>(`${bff.url}/products/${id}`, { body: JSON.stringify(data) }, true);
+};
 
 export const useProductRelatedGet = (id: string): Result<ProductRelated> =>
   useRequest(`${bff.url}/products/related/${id}`);
